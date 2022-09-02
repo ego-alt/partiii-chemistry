696ca0b3fea796af5707a68c564ffd1311bcf886
Ellery Gopaoco <batfolder@Ellerys-MacBook-Air.local>
2022-09-03 00:32:14 +0100

    Initial working model

---
+++ generate.py
@@ -0,0 +1,106 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import matplotlib.animation as animation
+
+
+class Grid:
+    def __init__(self, n, max_steps):
+        self.length = n
+        self.cell_status = [1, 2, 3]  # 0=dead, 1=type A, 2=type B, 3=type C
+        self.grid = self.initial_grid()
+        self.max_steps = max_steps
+        self.current_step_count = 0
+        self.album = []
+        self.img_frames = []
+
+        exchan_var = 0.8
+        reprod_var = select_var = 1
+        self.total_prob = exchan_var + reprod_var + select_var
+        exchan_prob = self.probability(exchan_var)  # Average probability of exchange in the next time step dt
+        reprod_prob = self.probability(reprod_var)  # Average probability of reproduction in the next time step dt
+        select_prob = self.probability(select_var)  # Average probability of selection in the next time step dt
+
+        self.diff_prob = [exchan_prob, reprod_prob, select_prob]
+        self.dispatcher = [self.exchange, self.reproduction, self.selection]
+
+    def call(self):
+        # Reiterates each time step until the step limit has been reached
+        while self.current_step_count < self.max_steps:
+            self.one_time_step()
+            self.current_step_count += 1
+            print(self.current_step_count)
+
+        self.save_image()
+
+    def initial_grid(self):
+        # Initialises the randomly populated grid
+        grid = np.random.randint(self.cell_status[0], self.cell_status[-1] + 1, size=(self.length, self.length))
+        return grid
+
+    def probability(self, variable):
+        # Calculates the probability of exchange, reproduction or selection
+        probability = variable / self.total_prob
+        return probability
+
+    def one_time_step(self):
+        reaction_count = 0
+        num_cells = self.length * self.length
+        while reaction_count < num_cells:
+            # Randomly chooses cell coordinates
+            x = random.randrange(len(self.grid))
+            y = random.randrange(len(self.grid))
+            cell_coord = [x, y]
+
+            # Randomly chooses one of four neighbours (up, down, left, right)
+            x_or_y = random.randint(0, 1)
+            neighbour_coord = cell_coord.copy()
+            neighbour_coord[x_or_y] = (neighbour_coord[x_or_y] + random.choice([-1, 1])) % self.length
+
+            reaction = random.choices(self.dispatcher, weights=self.diff_prob)[0]
+            reaction(cell_coord, neighbour_coord)
+            reaction_count += 1
+
+        self.album.append(np.copy(self.grid))
+
+    def exchange(self, cell_coord, neighbour_coord):
+        # Swaps positions with a neighbouring cell
+        x, y = cell_coord[0], cell_coord[1]
+        new_x, new_y = neighbour_coord[0], neighbour_coord[1]
+        self.grid[x][y], self.grid[new_x][new_y] = self.grid[new_x][new_y], self.grid[x][y]
+
+    def selection(self, cell_coord, neighbour_coord):
+        x, y = cell_coord[0], cell_coord[1]
+        new_x, new_y = neighbour_coord[0], neighbour_coord[1]
+        if self.grid[x][y]:
+            cell_index = (self.cell_status.index(self.grid[x][y]) + 1) % len(self.cell_status)
+            if self.grid[new_x][new_y] == self.cell_status[cell_index]:
+                self.grid[new_x][new_y] = 0
+
+    def reproduction(self, cell_coord, neighbour_coord):
+        x, y = cell_coord[0], cell_coord[1]
+        new_x, new_y = neighbour_coord[0], neighbour_coord[1]
+        if self.grid[x][y]:
+            if self.grid[new_x][new_y] == 0:
+                self.grid[new_x][new_y] = self.grid[x][y]
+
+    def save_image(self):
+        for ind, grid in enumerate(self.album):
+            cmap = ListedColormap(["black", "red", "blue", "yellow"])
+            image = plt.imshow(grid, cmap=cmap, interpolation='nearest', animated=True)
+            self.img_frames.append([image])
+            plt.axis('off')
+            plt.savefig(f"frame_{ind}.png")
+
+    def save_video(self):
+        fig = plt.figure()
+        anim = animation.ArtistAnimation(fig, self.img_frames, blit=True)
+        video.save("movie.mp4", writer=animation.FFMpegWriter(fps=60))
+        plt.close()
+
+
+test = Grid(200, 5)
+test.call()
+test.save_video()
+
