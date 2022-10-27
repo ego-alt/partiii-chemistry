6f7c04f7bec402e34791a01d343c7125a5ea4400
Ellery Gopaoco <batfolder@sjc172n99.joh.private.cam.ac.uk>
2022-10-27 21:00:23 +0100

    Loop over parameters

--- command.py
+++ command.py
@@ -1,8 +1,19 @@
 """Run in Python REPL"""
 # import julia
 # julia.install()
+# from julia.api import Julia
+# jl = Julia(compiled_modules=False)
 
 from julia import Pkg
 Pkg.activate("Project")
 from julia import Project
 from plot_data import Reader, Writer
+
+Project.pair_handler(Project.death, 0.4)
+
+for i in [0.05, 0.5, 5]:
+    Project.pair_handler(Project.exchange, i)
+    x = Project.call(100, 4000)
+    y = Writer(x)
+    y.save_video(f"e{i}d6.mp4")
+    y.save_state(Project.pair_handler(write=True), f"e{i}d6.pickle")
